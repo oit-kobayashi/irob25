106b7a84e2b1eea3a8cc358dcd832ad2721f6d44
kobayashi <hiroyuki.kobayashi@oit.ac.jp>
2025-12-09 17:04:50 +0900

    1209  in lecture work

--- mobrob-25/slsrv/main.py
+++ mobrob-25/slsrv/main.py
@@ -1,12 +1,48 @@
-from flask import Flask, request, jsonify
-import json
+import numpy as np
+from tinydb import TinyDB, Query
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-app = Flask(__name__)
+app = FastAPI()
+db = TinyDB('robot.json')
+Robot = Query()
+B = 0.34   # tread = diameter (34 cm)
 
-@app.route("/hello", methods=['POST', 'GET'])
-def hello():
-    return 'world'
+class Method(BaseModel):
+    method: str
+    dsl:    float
+    dsr:    float
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+def read_db_robot(id: int):
+    r = db.search(Robot.id == id)
+    if len(r) == 0:
+        return None
+    return r[0]
+
+
+@app.get("/robots/{id}")
+def read_robot(id: int):
+    return read_db_robot(id)
+
+@app.post("/robots/{id}")
+def update_robot(id: int, method: Method):
+    if method.method == 'move':
+        dsl = method.dsl
+        dsr = method.dsr
+        robot = read_db_robot(id)
+        pose = robot['pose']
+        th = pose[2][0]
+        dth = (dsr - dsl) / B
+        r = (dsr + dsl) / (2 * dth)
+        dx = (dsr + dsl) / 2 * np.cos(th + (dsr - dsl) / (2 * B))
+        dy = (dsr + dsl) / 2 * np.sin(th + (dsr - dsl) / (2 * B))
+        pose[0][0] += dx
+        pose[1][0] += dy
+        pose[2][0] += dth
+        robot['pose'] = pose
+        db.remove(Robot.id == id)
+        db.insert(robot)
+        return "ok"
+    else:
+        return {"error": -1}
